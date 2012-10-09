832557e7286c56579d5ee3568522aedb69e3b88c
hadashi <dev@hadashikick.jp>
2012-10-09 22:21:00 +0900

    expressのバージョンを3系に

--- app.js
+++ app.js
@@ -8,20 +8,24 @@ var express = require('express'),
     io      = require('socket.io'),
     fs      = require('fs');
 
-var app = module.exports = express.createServer();
+var app = module.exports = express();
 
 // Configuration
 
 
-log4js.addAppender(log4js.fileAppender('log/debug.log'), 'debug');
-log4js.addAppender(log4js.fileAppender('log/access.log'), 'access');
-log4js.addAppender(log4js.fileAppender('log/system.log'), 'error');
+log4js.configure({
+  appenders: [
+    { type: 'file', filename: 'log/debug.log', category: 'debug' }
+  , { type: 'file', filename: 'log/access.log', category: 'access'  }
+  , { type: 'file', filename: 'log/error.log', category: 'error'  }
+  ]
+});
 
 app.configure('development', function(){
   app.set('listen', 3000);
   app.set('socket.io', {
-    host: 'localhost:' + app.settings.listen,
-    listen: app
+    url: 'localhost:3001',
+    listen: 3001
   });
 
   app.use(express.logger(require('./debugLogger')));
@@ -31,14 +35,14 @@ app.configure('development', function(){
 app.configure('production', function(){
   app.set('listen', './tmp/sockets/express.sock');
   app.set('socket.io', {
-    host: 'hadashikick.jp:3100',
+    url: 'hadashikick.jp:3100',
     listen: 3101
   });
 
   app.use(log4js.connectLogger(log4js.getLogger('access'), {
     level: log4js.levels.INFO
   }));
-  app.use(express.errorHandler()); 
+  app.use(express.errorHandler());
 });
 
 app.configure(function(){
@@ -67,7 +71,6 @@ io.configure('development', function() {
 // libs
 
 require('./rakugaki')(app, io, ['index']);
-require('./helpers')(app);
 
 // Routes
 
@@ -76,11 +79,11 @@ app.get('/', function(req, res){
   });
 });
 
-app.listen(app.settings.listen, function() {
+console.log(app.listen(app.settings.listen, function() {
   if (typeof(app.settings.listen) == 'string') {
     fs.chmodSync(app.settings.listen, '777');
   }
-});
+}));
 
 console.log("Express server listening on %s in %s mode",
             app.settings.listen, app.settings.env);
