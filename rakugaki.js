7ac9ecdbaed07b56885d17f8800cea03ec049483
f-kubotar <dev@hadashikick.jp>
2013-11-03 01:22:08 +0900

    とりあえず動くようになった

--- rakugaki.js
+++ rakugaki.js
@@ -3,23 +3,23 @@ var fs     = require('fs')
   , mkdirp = require('mkdirp')
   , log4js = require('log4js')
   , logger = log4js.getLogger('debug');
-    
+
 (function() {
   var pngBase64Header = 'data:image/png;base64,';
 
-  var ImageData = (function () { 
+  var ImageData = (function () {
     var self = function(data) {
       this.data = data;
     };
-    
+
     self.prototype.isPNGBase64Encoded = function() {
       return this.data && this.data.lastIndexOf(pngBase64Header, 0) == 0;
     };
-    
+
     self.prototype.decode = function(data) {
       return new Buffer(this.data.substr(pngBase64Header.length), 'base64');
     };
-    
+
     return self;
   })();
 
@@ -88,7 +88,7 @@ var fs     = require('fs')
             io.emit('painters count update', paintersCount);
           });
         });
-      })(io.of(ns));  
+      })(io.of(ns));
     });
   };
 })();
